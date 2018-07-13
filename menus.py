94c793040281e8e13e27f1833eb023bdf822370f
Elliot <34190415+ElliotNOTINLondon@users.noreply.github.com>
2018-07-14 00:06:54 +0100

    Tutorial pt.10 complete
    Load/save system and main menu implemented

--- menus.py
+++ menus.py
@@ -39,3 +39,19 @@ def inventory_menu(con, header, inventory, inventory_width, screen_width, screen
         options = [item.name for item in inventory.items]
 
     menu(con, header, options, inventory_width, screen_width, screen_height)
+
+
+def main_menu(con, background_image, screen_width, screen_height):
+    libtcod.image_blit_2x(background_image, 0, 0, 0)
+
+    libtcod.console_set_default_foreground(0, libtcod.light_yellow)
+    libtcod.console_print_ex(0, int(screen_width / 2), int(screen_height / 2) - 4, libtcod.BKGND_NONE, libtcod.CENTER,
+                             'SludgeQuest')
+    libtcod.console_print_ex(0, int(screen_width / 2), int(screen_height - 2), libtcod.BKGND_NONE, libtcod.CENTER,
+                             'designed by the Supreme Peasant')
+
+    menu(con, '', ['New game', 'Continue', 'Quit'], 24, screen_width, screen_height)
+
+
+def message_box(con, header, width, screen_width, screen_height):
+    menu(con, header, [], width, screen_width, screen_height)

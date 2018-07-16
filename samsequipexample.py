f3dae8bf72236bcda79d5f9c3018cfed1fcf3fe8
Elliot <34190415+ElliotNOTINLondon@users.noreply.github.com>
2018-07-16 12:49:20 +0100

    Add files via upload
    Added some pseudocode to be implemented for handling multiple pieces of equipment, with multiple bonuses, in different slots.

---
+++ samsequipexample.py
@@ -0,0 +1,12 @@
+class Equip
+	def __init__(self, slot, bonuses)
+		self.slot = slot
+		self.bonuses = bonuses	
+
+	def equip
+		if self.slot == 'HEAD' and not self.slot_equipped
+			self.slot_equipped
+		if self.slot == 'LEGS' and not self.slot_equipped
+			self.slot_equipped
+		
+		

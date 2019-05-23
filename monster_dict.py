daeb43c01571f48db12d6663e7772e95cd6ad80e
Elliot <34190415+ElliotNOTINLondon@users.noreply.github.com>
2019-05-23 15:06:55 +0100

    Added various new monstes (not yet implemented)
    - Added new factions: Beasts, Cultists, Cleansing Hand
    - Added Sludge Fiend: low-level enemy that is slightly stronger than the Wretch
    - Added Risen Sacrifice: fairly weak enemy which (in future) other cultist units may create from corpses
    - Added Eternal Kidnapper: fairly weak enemy which (in future) will knock unconscious on critical hit, transporting the player the the Palace of Hedonism dungeons (future Chamber). The plan is that the player can either escape, or if they are confident enough, progress through the unique encounter to face various challenges and bosses with excellent loot.
    - Added Cleansing Hand Crusader: moderate strength enemy representing a generic Cleansing Hand mob
    - Added Unique Enemy (miniboss) Alfonrice, the Spinning Blade: strong enemy which (when implemented) can parry, inflict bleed, sidestep attacks and generally be a nasty melee-focused encounter

--- monster_dict.py
+++ monster_dict.py
@@ -1,10 +1,14 @@
-from random import choice
 from fighter import Fighter
 from ai import *
 from entity import Entity
 from render_functions import RenderOrder
 
+'''
+COMMON ENEMIES 
+'''
 
+
+# PLANTS
 def whip_vine(x, y):
     # Whip Vine
     fighter_component = Fighter(current_hp=8, max_hp=8, damage_dice=1, damage_sides=2,
@@ -18,9 +22,10 @@ def whip_vine(x, y):
                   faction='Plants')
 
 
+# SCAVENGERS
 def wretch(x, y):
-    fighter_component = Fighter(current_hp=6, max_hp=6, damage_dice=1, damage_sides=3,
-                                strength=3, agility=0, vitality=1, intellect=1, perception=1, xp=50)
+    fighter_component = Fighter(current_hp=4, max_hp=4, damage_dice=1, damage_sides=3,
+                                strength=2, agility=1, vitality=1, intellect=1, perception=1, xp=30)
     ai_component = Aggressive()
     return Entity(x, y, 'w', libtcod.darker_red, 'Wretch',
                   'A stunted human swaddled in filthy rags and long since driven feral by the SludgeWorks.',
@@ -28,6 +33,102 @@ def wretch(x, y):
                   faction='Scavengers')
 
 
+def sludge_fiend(x, y):
+    fighter_component = Fighter(current_hp=9, max_hp=9, damage_dice=3, damage_sides=2,
+                                strength=2, agility=2, vitality=1, intellect=1, perception=1, xp=50)
+    ai_component = Aggressive()
+    return Entity(x, y, 'f', libtcod.dark_red, 'Sludge Fiend',
+                  'The irony of attempting to retain one\'s humanity whilst simultaneously seeking to consume '
+                  'all mutagenic material in one\'s path seems to be lost on this creature. Tattered clothing '
+                  'flows off this mutant\'s twisted form like a bullet-shredded cape, with obsidian spikes protruding '
+                  'in clusters from its gaunt, discoloured body.',
+                  blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component, ai=ai_component,
+                  faction='Scavengers')
+
+
+def thresher(x, y):
+        fighter_component = Fighter(current_hp=26, max_hp=26, damage_dice=3, damage_sides=5,
+                                    strength=5, agility=4, vitality=1, intellect=1, perception=1, xp=225)
+        ai_component = Aggressive()
+        return Entity(x, y, 'T', libtcod.dark_azure, 'Thresher',
+                      'A colossal ogre-like hominid covered in patches of matted hair and littered with scars. This '
+                      'creature tirelessly searches it\'s surroundings for new objects to smash together with a '
+                      'joyous, childlike expression.',
+                      blocks=True, fighter=fighter_component, render_order=RenderOrder.ACTOR, ai=ai_component,
+                      faction='Scavengers')
+
+
+def bloodseeker(x, y):
+    fighter_component = Fighter(current_hp=56, max_hp=56, damage_dice=4, damage_sides=6,
+                                strength=8, agility=4, vitality=1, intellect=1, perception=1, xp=500)
+    ai_component = Aggressive()
+    return Entity(x, y, 'B', libtcod.light_crimson, 'Bloodseeker',
+                  'An asymmetric monstrosity the size of a bear with a grinning, skinless snout. Rusted weaponry from '
+                  'previous encounters juts from it\'s hide like gruesome jewellery, with pale, twisted flesh creeping '
+                  'up the hilts. The creature\'s eyes are consumed by feral rage as it prowls the caverns, twitching '
+                  'from the eternal	state of pain inflicted by its inherent regeneration.',
+                  blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component,
+                  ai=ai_component, faction='Scavengers')
+
+
+# BEASTS
+def moire_beast(x, y):
+    fighter_component = Fighter(current_hp=14, max_hp=14, damage_dice=3, damage_sides=2,
+                                strength=3, agility=7, vitality=1, intellect=1, perception=1, xp=200)
+    ai_component = Aggressive()
+    return Entity(x, y, 'M', libtcod.light_grey, 'Moire Beast',
+                  'The hide of this squat quadruped is an affront to the senses; dense and intricate greyscale '
+                  'patterns constantly shift epileptically upon the beast\'s surface like a surrealist '
+                  'interpretation of a zebra. The gleam of it\'s fluorescent yellow, feline irises serve as the only '
+                  'ubiquitous reference point on this beast\'s wildyly fluctuating, migraine-inducing form.',
+                  blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component,
+                  ai=ai_component, faction='Beasts')
+
+
+# CULTISTS
+def risen_sacrifice(x, y):
+    fighter_component = Fighter(current_hp=8, max_hp=8, damage_dice=2, damage_sides=2,
+                                strength=2, agility=1, vitality=1, intellect=1, perception=1, xp=25)
+    ai_component = Aggressive()
+    return Entity(x, y, 'r', libtcod.lightest_fuchsia, 'Risen Sacrifice',
+                  'For those who have never encountered them, it is very easy to dismiss the Cult of Eternity as '
+                  'a sect of aimless madmen with an obsessive focus upon human sacrifice. Those who have seen the '
+                  'radiant bodies of the recently sacrificed reanimating joyfully with blood still flowing from a '
+                  'mortal wound would however strongly disagree with this statement.',
+                  blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component, ai=ai_component,
+                  faction='Cultists')
+
+
+def eternal_kidnapper(x, y):
+    fighter_component = Fighter(current_hp=14, max_hp=14, damage_dice=2, damage_sides=4,
+                                strength=3, agility=6, vitality=1, intellect=1, perception=1, xp=100)
+    ai_component = Aggressive()
+    return Entity(x, y, 'k', libtcod.light_fuchsia, 'Eternal Kidnapper',
+                  'By far the most notorious member of the Cult of Eternity and arguably serving the most '
+                  'necessary role within their hierarchy. Their mission is simple: Kidnap the most virginal '
+                  'entrants into the SludgeWorks so that the flow of flesh into the Palace of Hedonism is '
+                  'constant and plentiful. The way they creep through the caverns with their threatening iron '
+                  'blackjack makes this intention undeniably clear.',
+                  blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component, ai=ai_component,
+                  faction='Cultists')
+
+
+# CLEANSING HAND
+def cleansing_hand_crusader(x, y):
+    fighter_component = Fighter(current_hp=22, max_hp=22, damage_dice=3, damage_sides=4,
+                                strength=4, agility=4, vitality=1, intellect=1, perception=1, xp=50)
+    ai_component = Aggressive()
+    return Entity(x, y, 'C', libtcod.yellow, 'Cleansing Hand Crusader',
+                  'The staple foot soldier of the Cleansing Hand. With his bucket helm, emblazoned tabard and well-'
+                  'maintained platemail it is easy to see how these defenders of the faith are commonly known as '
+                  'crusaders. Their tactics, however, as anything but medieval - intimidatingly rigorous discipline '
+                  'combined with years of experience slaying deformed monstrosities leaves the crusaders fully able '
+                  'to hold their own against many daily challenges experienced within the SludgeWorks.',
+                  blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component, ai=ai_component,
+                  faction='Cleansing Hand')
+
+
+# HORRORS
 def hunchback(x, y):
     fighter_component = Fighter(current_hp=12, max_hp=12, damage_dice=1, damage_sides=12,
                                 strength=4, agility=0, vitality=1, intellect=1, perception=1, xp=125)
@@ -41,39 +142,20 @@ def hunchback(x, y):
                   ai=ai_component, faction='Horrors')
 
 
-def moire_beast(x, y):
-    fighter_component = Fighter(current_hp=18, max_hp=18, damage_dice=3, damage_sides=2,
-                                strength=3, agility=7, vitality=1, intellect=1, perception=1, xp=325)
+'''
+UNIQUE ENEMIES
+'''
+
+
+# CLEANSING HAND
+def alfonrice(x, y):
+    fighter_component = Fighter(current_hp=42, max_hp=42, damage_dice=4, damage_sides=4,
+                                strength=6, agility=6, vitality=1, intellect=1, perception=1, xp=750)
     ai_component = Aggressive()
-    return Entity(x, y, 'M', libtcod.light_grey, 'Moire Beast',
-                  'The hide of this squat quadruped is an affront to the senses; dense and intricate greyscale '
-                  'patterns constantly shift epileptically upon the beast\'s surface like a hyperbolically interpolated'
-                  'zebra. The gleam of it\'s fluorescent yellow, feline irises serve as the only ubiquitous '
-                  'reference point on this monster\'s migraine-inducing form.',
-                  blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component,
-                  ai=ai_component, faction='Scavengers')
-
-
-def thresher(x, y):
-        fighter_component = Fighter(current_hp=26, max_hp=26, damage_dice=3, damage_sides=4,
-                                    strength=5, agility=4, vitality=1, intellect=1, perception=1, xp=225)
-        ai_component = Aggressive()
-        return Entity(x, y, 'T', libtcod.dark_azure, 'Thresher',
-                      'A colossal ogre-like hominid covered in patches of matted hair and littered with scars. This '
-                      'creature tirelessly searches it\'s surroundings for new objects to smash together with a '
-                      'joyous, childlike expression.',
-                      blocks=True, fighter=fighter_component, render_order=RenderOrder.ACTOR, ai=ai_component,
-                      faction='Scavengers')
-
-
-def bloodseeker(x, y):
-    fighter_component = Fighter(current_hp=50, max_hp=50, damage_dice=4, damage_sides=6,
-                                strength=6, agility=3, vitality=1, intellect=1, perception=1, xp=500)
-    ai_component = Aggressive()
-    return Entity(x, y, 'B', libtcod.light_crimson, 'Bloodseeker',
-                  'An asymmetric monstrosity the size of a bear with a grinning, skinless snout. Rusted weaponry from '
-                  'previous encounters juts from it\'s hide like gruesome jewellery, with pale, twisted flesh creeping '
-                  'up the hilts. The creature\'s eyes are consumed by feral rage as it prowls the caverns, twitching '
-                  'from the eternal	state of pain inflicted by its inherent regeneration.',
-                  blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component,
-                  ai=ai_component, faction='Scavengers')
+    return Entity(x, y, 'A', libtcod.light_yellow, 'Alfonrice, the Spinning Blade',
+                  'The Cleansing Hand\'s most pious duelist Alfonrice earned his moniker not from the constant'
+                  'twirling of his offhand swordbreaker, but from his proficiency in dispatching hordes of filthy'
+                  'horrors with a single cleave of his cruciform broadsword. He grits his teeth in anticipation, '
+                  'anxious to cut down the next prospective entrant to the Most Holy Bastion.',
+                  blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component, ai=ai_component,
+                  faction='Cleansing Hand')
